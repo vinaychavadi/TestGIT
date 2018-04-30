3eef9180b04c6f4cfee8006e4f01c4db4aa65749
Vinay <vinay.chavadi@gmail.com>
2018-04-30 19:47:10 +0530

    In Minutes

--- TestGIT/Program.cs
+++ TestGIT/Program.cs
@@ -10,7 +10,36 @@ namespace TestGIT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!!");
+
+            string str = "10h";    
+            string[] arr = str.Split('h');
+            string hr = string.Empty;
+            string min = string.Empty;
+            int h = 0;
+            int m = 0;
+            int t = 0;
+            if (arr.Length > 1)
+            {
+                 h =  int.Parse(arr[0].ToString()) * 60;
+                hr = h.ToString();
+                if (arr.Length == 2)
+                {
+                    if (arr[1].ToString() != String.Empty)
+                    {
+                        string[] arr1 = arr[1].ToString().Split('m');
+                        m = int.Parse(arr1[0].ToString());
+                    }
+                }
+                
+            }
+            else
+            {
+                string[] arr1 = str.Split('m');
+                 m = int.Parse(arr1[0].ToString()) ;
+                
+            }
+            t = h + m;
+
         }
     }
 }
