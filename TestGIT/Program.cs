0be14bf0e986501223c1451d16b9becf9f50054a
Vinay <vinay.chavadi@gmail.com>
2019-03-18 19:05:38 +0530

    MYT-6  #comment New Line added

--- TestGIT/Program.cs
+++ TestGIT/Program.cs
@@ -4,52 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Net.Http;
+
 namespace TestGIT
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int tm = 160;
-            TimeSpan time = TimeSpan.FromMinutes(tm);
-            string str1 = time.ToString("h:mm");
-            string str = "10h";    
-            string[] arr = str.Split('h');
-            string hr = string.Empty;
-            string min = string.Empty;
-            int h = 0;
-            int m = 0;
-            int t = 0;
-            //Test gain
-            if (arr.Length > 1)
-            {
-                 h =  int.Parse(arr[0].ToString()) * 60;
-                hr = h.ToString();
-                if (arr.Length == 2)
-                {
-                    if (arr[1].ToString() != String.Empty)
-                    {
-                        string[] arr1 = arr[1].ToString().Split('m');
-                        m = int.Parse(arr1[0].ToString());
-                    }
-                }
-                //these are new comments
-                //New Comments today ...
-                
-            }
-            else
-            {
-                string[] arr1 = str.Split('m');
-                 m = int.Parse(arr1[0].ToString()) ;
-                
-            }
-            t = h + m;
-            //Testing
-            //New Comments
-            //resolve the ticket
-            //MYF-5 #time 2h Hunting that bug. #resolve-issue
-            //Add Comments!!
+            MakeRequest();
 
+
+           
+
+        }
+
+        private static async Task MakeRequest()
+        {
+            Console.WriteLine("Making Request");
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(new Uri("https://247-dev.atlassian.net/rest/webhooks/webhook1"));
+            var body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
+            Console.ReadLine();
         }
     }
 }
