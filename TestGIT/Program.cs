8bb67200ca62b57009dc42e95714d4a25c852943
Vinay <vinay.chavadi@gmail.com>
2019-03-19 16:31:31 +0530

    MYT-6  #comment New Line added

--- TestGIT/Program.cs
+++ TestGIT/Program.cs
@@ -21,7 +21,7 @@ namespace TestGIT
 
         private static async Task MakeRequest()
         {
-            Console.WriteLine("Making Request again with Changes!!!");
+            Console.WriteLine("Making Request again with Changes");
             var httpClient = new HttpClient();
             var response = await httpClient.GetAsync(new Uri("https://247-dev.atlassian.net/rest/webhooks/webhook1"));
             var body = await response.Content.ReadAsStringAsync();
